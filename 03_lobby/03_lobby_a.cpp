4348b92ba63bde6caa64a7f52f3ebda0f957bc5d
Spheya <tamara@spheya.com>
2025-12-03 11:34:21 +0100

    day 3

---
+++ 03_lobby/03_lobby_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <stringutils.hpp>
+#include <fileutils.hpp>
+
+int main() {
+	std::string input = loadFileToString("./03_lobby.txt");
+	uint64_t result = 0;
+
+	for (SubStringIterator it(input, "\r\n"); it; ++it) {
+		auto firstBattery = std::max_element(it->begin(), it->end() - 1);
+		auto secondBattery = std::max_element(firstBattery + 1, it->end());
+		result += (*firstBattery - '0') * 10 + (*secondBattery - '0');
+	}
+
+	std::cout << result << std::endl;
+}
---
+++ 03_lobby/03_lobby_b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <stringutils.hpp>
+#include <fileutils.hpp>
+
+int main() {
+	std::string input = loadFileToString("./03_lobby.txt");
+	uint64_t result = 0;
+
+	for (SubStringIterator it(input, "\r\n"); it; ++it) {
+		uint64_t multiplier = 100000000000;
+
+		auto start = it->begin();
+		for (int i = 11; i >= 0; --i, multiplier /= 10) {
+			auto battery = std::max_element(start, it->end() - i);
+			start = battery + 1;
+			result += (*battery - '0') * multiplier;
+		}
+	}
+
+	std::cout << result << std::endl;
+}
