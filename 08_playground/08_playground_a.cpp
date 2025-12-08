492a4cd247469157d71398d4414c201cf40adc25
Spheya <tamara@spheya.com>
2025-12-08 11:12:47 +0100

    day 8

---
+++ 08_playground/08_playground_a.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include <stringutils.hpp>
+#include <fileutils.hpp>
+
+struct Point {
+	unsigned circuitId;
+	int64_t x, y, z;
+};
+
+struct Connection {
+	Point* a;
+	Point* b;
+	uint64_t dstSq;
+};
+
+int main() {
+	std::string input = loadFileToString("./08_playground.txt");
+
+	std::vector<Point> points;
+	for (StringTokenizer it(input, "\r\n"); it; ++it) {
+		StringTokenizer coord(*it, ",");
+		int64_t x = std::stoll(std::string(*coord++));
+		int64_t y = std::stoll(std::string(*coord++));
+		int64_t z = std::stoll(std::string(*coord++));
+		points.push_back({0, x, y, z});
+	}
+
+	std::vector<Connection> connections;
+	for (auto it1 = points.begin(); it1 != points.end(); ++it1) {
+		for (auto it2 = it1 + 1; it2 != points.end(); ++it2) {
+			connections.push_back(Connection{&*it1, &*it2, uint64_t((it1->x - it2->x) * (it1->x - it2->x) + (it1->y - it2->y) * (it1->y - it2->y) + (it1->z - it2->z) * (it1->z - it2->z))});
+		}
+	}
+
+	std::sort(connections.begin(), connections.end(), [](const Connection& a, const Connection& b){ return a.dstSq < b.dstSq; });
+
+	unsigned nextCircuitId = 1;
+	for (int i = 0; i < 1000; ++i) {
+		const Connection& connection = connections[i];
+		Point* a = connection.a;
+		Point* b = connection.b;
+		if(a->circuitId > b->circuitId) std::swap(a, b);
+
+		if (b->circuitId == 0) {
+			a->circuitId = nextCircuitId;
+			b->circuitId = nextCircuitId;
+			++nextCircuitId;
+		} else if (a->circuitId == 0) {
+			a->circuitId = b->circuitId;
+		} else if (a->circuitId != b->circuitId) {
+			unsigned swapId = a->circuitId;
+			for(auto& p : points)
+				if(p.circuitId == swapId)
+					p.circuitId = b->circuitId;
+		}
+	}
+
+	std::vector<unsigned> circuitSizes(nextCircuitId);
+	for(auto& p : points)
+		if(p.circuitId != 0)
+			++circuitSizes[p.circuitId];
+
+	std::sort(circuitSizes.rbegin(), circuitSizes.rend());
+	std::cout << circuitSizes[0] * circuitSizes[1] * circuitSizes[2] << std::endl;
+}
+
